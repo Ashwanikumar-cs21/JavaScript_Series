ef11b0b4c4826910e3c5e22acae5bbd0e1d4289e
Ashwani Kumar <105841839+Ashwanikumar-cs21@users.noreply.github.com>
2024-04-04 10:46:41 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all js code newer version 
+
+
+//alert("hello")  //we are using nodejs not browser alert reflets only on browser;
+
+console.log(3  
+    
+    
+    +3); // code readablity should be high 
+
+
+console.log("Ashwani");
+
+
+// DataTypes
+
+let Name = "Ashwani" //String
+let age = 18 //Number 
+let isLoggedIn = true //Boolean
+let state ;//undefined
+let states = null
+
+
+
+
+//bigint
+//string = ""
+//boolean = true or false
+//null => standalone value 
+//undefined => value is not assigned
+//symbol => use for uniqueness
+
+
+
+//object
+
+console.log(typeof "Ashwani") // string
+console.log(typeof null); // object
+console.log(typeof undefined);  // indefined
