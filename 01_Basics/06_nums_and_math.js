da7a3d53c436a5f28a4e115b93c974f8e9b7d699
Ashwani Kumar <105841839+Ashwanikumar-cs21@users.noreply.github.com>
2024-04-13 13:13:34 +0000

    Nums and Maths

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance);
+
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+
+const num = 21.895
+// console.log(num.toPrecision(3));
+
+
+const zeros = 100000000;
+// console.log(zeros.toLocaleString('en-IN'));
+
+
+
+
+//_____________________________Maths_______________________________________________
+
+console.log(Math);
