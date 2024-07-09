da8da8ac683a0f2766c2602d71c24713274d88c8
Ashwani Kumar <105841839+Ashwanikumar-cs21@users.noreply.github.com>
2024-07-09 09:09:53 +0000

    Scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,14 @@
+// const a = 10
+// let b = 20
+// var c = 30
+// console.log(a,b,c);
+
+if(true){
+    const a = 10
+    let b = 20
+    var c = 30
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
