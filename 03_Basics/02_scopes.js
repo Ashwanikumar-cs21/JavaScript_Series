0db5a0680b38f9f4f08ed311da2682f1024b479f
Ashwani Kumar <105841839+Ashwanikumar-cs21@users.noreply.github.com>
2024-07-09 19:13:48 +0000

    Scopes in functions

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -9,6 +9,59 @@ if(true){
     var c = 30
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+let a = 300
+if(true){
+    const a = 10
+    let b = 20
+    // console.log("Inner :",a);
+}
+
+// console.log(a);
+
+
+// nested scopes
+
+function FuncOne(){
+    const UserName = "Ashwani"
+
+    function FuncTwo(){
+        const website = "Github"
+        console.log(UserName)
+    }
+    // console.log(website)
+
+    FuncTwo()
+}
+// FuncOne()
+
+
+if(true){
+    const UserName = "Ashwani"
+    if(UserName == "Ashwani"){
+        const website = " Github"
+        // console.log(UserName + website);
+    }
+    // console.log(website);
+}
+// console.log(UserName);
+
+
+
+// ++++++++++++++++++++++++++++imp++++++++++++++++++++++++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
